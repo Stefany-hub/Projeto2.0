9881d24516a56a67345fc5e568301663f6000f8a
yurivedovate-bandtec <yuri.vedovate@bandtec.com.br>
2020-06-10 12:31:27 -0300

    atualização do script do banco

--- banco de dados/banco novo/banco/script/script da modelagem.sql
+++ banco de dados/banco novo/banco/script/script da modelagem.sql
@@ -1,5 +1,6 @@
 CREATE database MIT;
 USE MIT;
+
 CREATE TABLE Cliente (
   idCliente INT primary key,
   CNPJ CHAR(13) NULL,
@@ -13,23 +14,23 @@ CREATE TABLE Endereço (
   );
 
 CREATE TABLE Lixeira (
-  idLixeira INT primary key,
+  idLixeira INT,
   Cliente_idCliente INT,
-  IdSensorEntrada CHAR(5) ,
-  IdSensor1 CHAR(5) ,
-  IdSensor2 CHAR(5) ,
-  IdSensor3 CHAR(5) ,
+  IdSensorEntrada CHAR(5),
+  IdSensor1 CHAR(5),
+  IdSensor2 CHAR(5),
+  IdSensor3 CHAR(5),
+  primary key (idLixeira,IdSensorEntrada,IdSensor1,IdSensor2, IdSensor3),
   Endereço_idEndereço INT,
-FOREIGN KEY (Cliente_idCliente)REFERENCES Cliente (idCliente),
-FOREIGN KEY (Endereço_idEndereço)REFERENCES Endereço (idEndereço)
+  FOREIGN KEY (Cliente_idCliente)REFERENCES Cliente (idCliente),
+  FOREIGN KEY (Endereço_idEndereço)REFERENCES Endereço (idEndereço)
 );
-
 
 CREATE TABLE StatusSensor(
   IdMovimentacao INT primary key,
   NivelTotal INT NULL,
   DataHora DATETIME NULL,
   Lixeira_idLixeira INT,
-FOREIGN KEY (Lixeira_idLixeira) REFERENCES Lixeira (idLixeira)
+  FOREIGN KEY (Lixeira_idLixeira) REFERENCES Lixeira (idLixeira)
 );
 
