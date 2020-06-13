0013441f475eefd0b27af4bf1cd6dbac20007cc9
yurivedovate-bandtec <yuri.vedovate@bandtec.com.br>
2020-06-13 17:20:58 -0300

    organização do repositorio

--- Apis-professores/api marise/node_database/node_database/NODE4 (1)/app/database.js
+++ Apis-professores/api marise/node_database/node_database/NODE4 (1)/app/database.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
     port     : '3306',
-    user     : 'stefany',
-    password : 'urubu100',
+    user     : 'yuri',
+    password : '221015',
     database : 'trc5000'
 });
 
